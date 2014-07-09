42ed07f98bf760c689537405369365d50a40603b
Jessica Owens <jessicadanaowens@gmail.com>
2014-07-09 09:53:41 -0600

    first test

---
+++ lib/dog_hash.rb
@@ -0,0 +1,24 @@
+
+
+  DOG = {:cattle_dog =>{:preferences => [{:name => "Harleigh"}]}}
+
+
+
+
+
+#
+# p DOG[:cattle_dog].first
+# # expect(DOG[:cattle_dog][:preferences].first[:name]).to eq("Harleigh")
+# # end
+# #
+# # expect(DOG[:cattle_dog][:names]).to eq(['Harleigh', 'brown'])
+#
+# @dogs = [
+#   {name: "Fido",     size: :large,  owner: joe},
+#   {name: "Yapper",   size: :small,  owner: joe},
+#   {name: "Bruiser",  size: :large,  owner: joe},
+#   {name: "Tank",     size: :huge,   owner: sarah},
+#   {name: "Beast",    size: :large,  owner: sarah},
+#   {name: "Harleigh", size: :medium, owner: andrew},
+#   {name: "Trixie",   size: :small,  owner: andrew}
+# ]
