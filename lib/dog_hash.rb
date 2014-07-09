f748948e6d093222c49c16447c7fe266181b1778
Jessica Owens <jessicadanaowens@gmail.com>
2014-07-09 10:07:10 -0600

    all tests passing

--- lib/dog_hash.rb
+++ lib/dog_hash.rb
@@ -1,6 +1,9 @@
 
 
-  DOG = {:cattle_dog =>{:preferences => [{:name => "Harleigh"}]}}
+  DOG = {:cattle_dog =>{:preferences => [{:name => "Harleigh"}], :names => ['Harleigh', 'brown']},
+         'harleigh'=> {:preferences => [{:favorite_toy => "frog"}], :information => {:sport => ["frisbee", "hiking"]}},
+         'trixie' => {:preferences => [:favorite_food => "steak"], :information => {:sport => [:swimming, :prancing]}}
+  }
 
 
 
